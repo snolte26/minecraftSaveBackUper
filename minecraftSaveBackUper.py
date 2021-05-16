8ae20b1e3cab3edaba24163da7341286c1aed911
Cepha <84254852+QueenCepha@users.noreply.github.com>
2021-05-16 15:24:47 -0400

    moves the zipping into its own function so other backup functions can call it

--- minecraftSaveBackUper.py
+++ minecraftSaveBackUper.py
@@ -3,6 +3,20 @@ import shutil
 
 #Define Funtions
 
+def zip(source, destination, world):
+    # adds the world name to the source/destinations
+    source = source + "\\" + world
+    destination = destination + "\\" + world
+    
+    # copies the world to the desktop
+    dest = shutil.copytree(source, destination)
+    # creates a .zip of the world thats on the desktop
+    shutil.make_archive(destination, "zip", destination)
+    
+    # closes the document after all is done
+    src.close()
+
+
 def backUp(src):
     
     # goes through the document, setting the source and
@@ -23,17 +37,10 @@ def backUp(src):
     world = input("Enter the name of the world you want to back up exactly: ")
     print("Working...")
     
-    # adds the world name to the source/destinations
-    source = source + "\\" + world
-    destination = destination + "\\" + world
+    # Zip the chosen world onto the desktop
+    zip(source, destination, world)
     
-    # copies the world to the desktop
-    dest = shutil.copytree(source, destination)
-    # creates a .zip of the world thats on the desktop
-    shutil.make_archive(destination, "zip", destination)
     
-    # closes the document after all is done
-    src.close()
 
 
 
@@ -58,11 +65,13 @@ def setup():
     # set the world save directory to the user input with a line break
     # same with desktop directory
     saves = input("Enter world saves directory (ex: C:\\Users\\Your_UserName\\AppData\\Roaming\\.minecraft\\saves): ") + "\n"
-    desktop = input("Enter output directory (C:\\Users\\Your_UserName\\Desktop): ") + "\n"
+    desktop = input("Enter output directory (ex: C:\\Users\\Your_UserName\\Desktop): ") + "\n"
+    game = input("Enter .minecraft directory (ex: C:\\Users\\Your_UserName\\AppData\\Roaming\\.minecraft): ") + "\n"
     
     # writes the saves and desktop variables to the document
     saveDirectory.writelines(saves)
     saveDirectory.writelines(desktop)
+    saveDirectory.writelines(game)
     
     # closes the text document, then reopens as read only
     saveDirectory.close()
@@ -77,18 +86,22 @@ def menu():
         print("What would you like to do?")
         print("1. Set Saves and Desktop locations")
         print("2. Back up a single world")
+        print("3. Back up all worlds")
         print("69. Close program")
         print()
         option = int(input("Enter the number of your choice: "))
         
+        # run the setup() function
         if option == 1:
             setup()
             
+        # Open saveDirectory.txt in read only as src, and send it to backUp()
         if option == 2:
             src = open("saveDirectory.txt", "r")
             backUp(src)
             
-        if option == 69:
+        # error closes the program
+        if option == 69: #69 nice
             askdhfjalohrg
         
         #loop to start of menu if an invalid option is selected
@@ -101,11 +114,10 @@ def main():
     
     # runs the fileExists() function, and sets result to exists variable, Boolean
     exists = fileExists()
-    # If true, open saveDirectory.txt in read only as src, and send it to backUp()
+    
     if exists:
         menu()
-        #src = open("saveDirectory.txt", "r")
-        #backUp(src)
+        
     # if false, run the setup() function
     else:
         setup()
